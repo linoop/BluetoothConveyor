67239ea69fc30e06bd33ed7a273d49a77c1c6bbc
linoop <linoop@technowavegroup.com>
2022-07-05 10:06:28 +0530

    -keep public class MyClass

--- app/src/main/java/com/linoop/btconveyorlib/MainActivity.kt
+++ app/src/main/java/com/linoop/btconveyorlib/MainActivity.kt
@@ -2,8 +2,10 @@ package com.linoop.btconveyorlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.technowavegroup.printerlib.BTUtil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btUtil: BTUtil
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
